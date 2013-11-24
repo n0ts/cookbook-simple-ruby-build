100a89cf7cb7e492064bda5314d164d00e3bb13c
n0ts <me@n0ts.org>
2013-11-24 19:44:36 +0900

    Fixed a default ruby version

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default['simple-ruby-build']['ruby_version'] = "2.0.0-p253"
+default['simple-ruby-build']['ruby_version'] = "2.0.0-p353"
 default['simple-ruby-build']['aws_region'] = "us-east-1"
 default['simple-ruby-build']['aws_s3_bucket'] = "simple-ruby-build"
 default['simple-ruby-build']['auto_shutodwn'] = true
